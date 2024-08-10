8f81f874388e2c9d8d83f2176245b9bfe2bc2686
Suhail Sharieff <suhailsharieffsharieff@gmail.com>
2024-08-10 17:16:25 +0530

    made port as 5000

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ import { fileURLToPath } from 'url';
 dotenv.config();
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
