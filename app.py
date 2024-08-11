53f15ee42ab4a1343b59a67ccac1723e0d25b40c
yash-r04 <150778551+yash-r04@users.noreply.github.com>
2024-08-11 08:34:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -54,5 +54,5 @@ def location():
         'longitude': longitude,
     })
 
-if __name__ == '__main__':
-    app.run(debug=True,port = 5000)
+#if __name__ == '__main__':
+   # app.run(debug=True,port = 5000)
